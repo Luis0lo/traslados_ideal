937e6523f7f62ce0b188f3ce9054a566ab2e4cff
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-10-12 10:45:12 +0100

    social whatsapp

--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -1,11 +1,24 @@
-import { BsInstagram } from 'react-icons/bs';
+import { BsInstagram, BsWhatsapp } from 'react-icons/bs';
 import './socialMedia.css';
 
 function SocialMedia() {
   return (
     <div className="socialContainer">
-      <a href="https://www.instagram.com/traslados_ideal/">
-        <BsInstagram size="40" />
+      <a
+        aria-label="Visit Instagram account"
+        href="https://www.instagram.com/traslados_ideal/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <BsInstagram className="iconI" />
+      </a>
+      <a
+        aria-label="Chat on WhatsApp"
+        href="https://wa.me/584242426636"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <BsWhatsapp className="iconW" />
       </a>
     </div>
   );
