a00c9107d30c8b3ca3bcaaa33e756ed66ea8fa5f
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-11-04 17:38:13 +0000

    handle languages !== en-es

--- public/index.js
+++ public/index.js
@@ -2,6 +2,10 @@ let userLocale =
   navigator.languages && navigator.languages.length
     ? navigator.languages[0]
     : navigator.language;
-const userLanguage = userLocale.split('-')[0];
+let userLanguage = userLocale.split('-')[0];
 
-document.documentElement.setAttribute('lang', userLanguage);
+if (userLanguage !== 'es' || userLanguage !== 'en') {
+  document.documentElement.setAttribute('lang', 'en');
+} else {
+  document.documentElement.setAttribute('lang', userLanguage);
+}
